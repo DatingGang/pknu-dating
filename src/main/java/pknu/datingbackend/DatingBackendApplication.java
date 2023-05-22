1f9a359e831779184b16c47014b5aea8cc4cb96f
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-05-22 17:01:37 +0900

    config: 초기 프로젝트 설정

---
+++ src/main/java/pknu/datingbackend/DatingBackendApplication.java
@@ -0,0 +1,13 @@
+package pknu.datingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pknu/datingbackend/DatingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pknu.datingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
